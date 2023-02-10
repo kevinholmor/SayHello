585460d69eeec6a4c39cef47e000875c04262c96
kevinholmor <124747273+kevinholmor@users.noreply.github.com>
2023-02-09 18:21:33 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Scanner reader = null;
+        int num1 = 2;
+
+        int num2 = 2;
+
+        int resultado = num1 + num2;
+
+        System.out.println("El resultado de la suma es: " + resultado);
+    }
+}
